868aa517ebb8c59ac51f68ae92370f7ee2ce474e
Umit Turk <uyt.turk@gmail.com>
2022-04-08 23:55:53 +0300

    views and ejs created navigation is done

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
+//express sayesinde çok kolaylıkla http metotlarını kullanabiliriz.
+const app = express()
+
+//TEMPLATE ENGINE
+app.set('view engine','ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'))
+
+//ROUTES
+app.get('/',(req, res) => {
+    res.render("index");
+});
+
+app.get('/about',(req, res) => {
+    res.render('about');
+})
+
+app.get('/add',(req, res) => {
+    res.render('add_post');
+})
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`server ${port} unda başlatıldı`);
+})
